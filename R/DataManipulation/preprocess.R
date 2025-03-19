be45ea35dca3802fc6a29698a63dfe597183ed2e
imn167 <imn167.tb@gmail.com>
2025-03-19 08:56:48 +0100

    ModelAge Comparaison started

---
+++ R/DataManipulation/preprocess.R
@@ -0,0 +1,15 @@
+### data used in paper
+SamplesNames <- c("OSL8", "OSL7" ,"OSL6", "OSL4", "OSL1")
+ddot = c( 1.40, 1.30, 1.25, 1.33, 1.13)
+sddot <- rep(.03, 4)
+sdc <- c(0.08, 0.09, 0.09, 0.09, 0.10)
+alpha <- rep(1, 4)
+
+DATA4 <-  combine_DataFiles(DATA1, DATA2, DATA3)
+DATA4$SampleNames <-  c("GDB3", "GDB5", "FER1")
+
+P <- Palaeodose_Computation(DATA4, DATA4$SampleNames, DATA4$Nb_sample)
+
+DATA4$ddot_env
+DtMeasures <- create_MeasuresDataFrame(P, DATA4,alpha[1], sdc[1:3])
+DATA4$ddot_env
