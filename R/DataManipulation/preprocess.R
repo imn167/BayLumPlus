c90182bc86c250a0a9bcca2441a5312fcd1b2f16
imn167 <imn167.tb@gmail.com>
2025-03-20 17:15:00 +0100

    ==== GibbsSampler in progress / problem in density output / change DATAMeasures format ===

--- R/DataManipulation/preprocess.R
+++ R/DataManipulation/preprocess.R
@@ -13,12 +13,69 @@ P <- Palaeodose_Computation(DATA4, DATA4$SampleNames, DATA4$Nb_sample)
 
 DtMeasures <- create_MeasuresDataFrame(P, DATA4,alpha[1], sdc[1:3])
 DtMeasures$Measures$D
-Sc = matrix(c(rep(1, 3), c(0,1,1), c(0,0,1), rep(0,3)), nrow = 4, byrow = T) #change it !!
+Sc = matrix(c(rep(1, 3), c(0,1,1), c(0,0,1), rep(0,3)), nrow = 4, byrow = T)
 
 AgeAsBayLum <-Compute_AgeS_D(DtMeasures, Sc, ModelAgePrior$Jeffreys)
 
 AgeCorrected <- Compute_AgeS_D(DtMeasures, Sc, prior = "StrictOrder")
-AgeAsBayLum$Ages
-AgeCorrected$Ages
+
+
+GibbsOutput = GibbsSampler(DtMeasures, rep(.01, 3), 10000, 2000,Sc, 1, 100)
+
+
+
+###-------------------------------------------------------------------------------@
+C14_SampleNames = c("OxA-21261","OxA-21262","UCIAMS-98210","UCIAMS-103134","P-782","UCIAMS-103138","OxA-23247","OxA-9774",
+                    "OxA-9946","UCIAMS-98208","UCIAMS-98209",
+                    "OxA-9947","OxA-9775","OxA-9948","OxA-23523","UCIAMS-103135","UCIAMS_103136","UCIAMS-103137",
+                    'OxA-23248',"UCIAMS-103141","UCIAMS-103140",
+                    "OxA-27087","OxA-9949","UCIAMS-103139","OxA-23249","OxA-9950","UCIAMS-109991","OxA-23250","UCIAMS-109992","OxA-9776",
+                    "OxA-23251","UCIAMS-109993","OxA-9892","OxA-9777","UCIAMS-109994","OxA-9778","OxA-23252","OxA-9893","PL-980525A","AA-27982")
+
+SC = matrix(data=0,ncol=40,nrow=41) ### matrix to account for stratigraphic constraints
+SC[1,]=rep(1,40)
+SC[2,]=c(rep(0,1),rep(1,1),rep(0,3),rep(1,35)) #4853
+SC[3,]=c(rep(0,5),rep(1,35)) #4861
+SC[4,]=c(rep(0,5),rep(1,35)) #4555
+SC[5,]=c(rep(0,5),rep(1,35)) #4779
+SC[6,]=rep(0,40) #P782 After
+SC[7,]=c(rep(0,9),rep(1,31)) #4850
+SC[8,]=c(rep(0,9),rep(1,31)) #4850
+SC[9,]=rep(0,40) #4715 (After)
+SC[10,]=rep(0,40) #4715 (After)
+SC[11,]=c(rep(0,18),rep(1,4),rep(0,1),rep(1,17)) #4517
+SC[12,]=c(rep(0,18),rep(1,4),rep(0,1),rep(1,17)) #4517
+SC[13,]=rep(0,40)#4822 (After)
+SC[14,]=rep(0,40) #4826 (After)
+SC[15,]=rep(0,40) #4826 (After)
+SC[16,]=c(rep(0,16),rep(1,2),rep(0,4),rep(1,1),rep(0,1),rep(1,16)) #4828
+SC[17,]=c(rep(0,16),rep(1,2),rep(0,4),rep(1,1),rep(0,1),rep(1,16)) #4828
+SC[18,]=c(rep(0,17),rep(1,1),rep(0,4),rep(1,1),rep(0,1),rep(1,16)) #4836
+SC[19,]=c(rep(0,22),rep(1,1),rep(0,1),rep(1,16)) #4837
+SC[20,]=c(rep(0,19),rep(1,2),rep(0,1),rep(1,18)) #4869
+SC[21,]=c(rep(0,19),rep(1,2),rep(0,1),rep(1,18)) #4869
+SC[22,]=c(rep(0,22),rep(1,18)) #4867
+SC[23,]=c(rep(0,22),rep(1,18)) #4867
+SC[24,]=c(rep(0,24),rep(1,16)) #4848
+SC[25,]=c(rep(0,24),rep(1,16)) #4865
+SC[26,]=c(rep(0,25),rep(1,15)) #4878
+SC[27,]=c(rep(0,26),rep(1,14)) #5276
+SC[28,]=c(rep(0,27),rep(1,13)) #5279
+SC[29,]=c(rep(0,29),rep(1,11)) #5283
+SC[30,]=c(rep(0,29),rep(1,11)) #5283
+SC[31,]=c(rep(0,30),rep(1,10)) #5292
+SC[32,]=c(rep(0,31),rep(1,9)) #5308
+SC[33,]=c(rep(0,32),rep(1,8)) #5316
+SC[34,]=c(rep(0,33),rep(1,7)) #5317
+SC[35,]=c(rep(0,35),rep(1,5)) #5323
+SC[36,]=c(rep(0,35),rep(1,5)) #5323
+SC[37,]=c(rep(0,36),rep(1,4)) #5324
+SC[38,]=c(rep(0,37),rep(1,3)) #5328
+SC[39,]=c(rep(0,38),rep(1,2)) #5329
+SC[40,]=rep(0,40) #pre XII (After)
+SC[41,]=rep(0,40) #pre XII (After)
+
+initialize_SC(SC, 1,100,T)
+
 
 
--- R/Gibbs.R
+++ R/Gibbs.R
@@ -1,27 +1,29 @@
-####
 
+#=================================================================================@
 #'
 #'@export
-findbound <- function(Sc, index,  UpperPeriod, LowerPeriod) {
+findbound <- function(index,  Sc) {
+  Sc = Sc[-1, ] #del lower bound line
 
   lowerbound_index = which(Sc[, index] == 1)
-  if (length(lowerbound_index) > 0) { a = A[min(lowerbound_index)]}
+  if (length(lowerbound_index) > 0) { a = max(lowerbound_index)}
 
-  else { a = LowerPeriod}
+  else { a = 0}
 
   upperbound_index = which(Sc[index, ] == 1)
-  if (length(upperbound_index) > 0 ) {b = A[max(upperbound_index)]}
-  else {b = UpperPeriod}
-  return(list(a = a, b = b))
+  if (length(upperbound_index) > 0 ) {b = min(upperbound_index)}
+  else {b = -1}
+  return(c(a,b))
 }
 
+#=================================================================================@
 #'@export
-GibbsDensity <- function(DataMeasures, A, Ai, index, SC, LowerPeriod, UpperPeriod) {
+GibbsDensity <- function(DataMeasures, A, Ai, index, a, b) {
     ## Using the function create_MeasuresDataFrame
   Measures <- DataMeasures$Measures
   D = Measures$D
   Di = D[index]
-  sD = Measures$Ds
+  sD = Measures$sD
   sDi = sD[index]
   ddot = Measures$ddot
   ddoti = ddot[index]
@@ -39,40 +41,148 @@ GibbsDensity <- function(DataMeasures, A, Ai, index, SC, LowerPeriod, UpperPerio
     1 + sum((A*ddot * alpha)**2 / ((A*sddot)**2 + sD**2))
   }
 
-  invdiagvar = ((Ai*sddoti)**2 + sDi**2)**(-1)
-  centeri = (Di-Aiddoti)**2
 
-  firstExp = exp( (-.5) * ( (centeri *invdiagvar ) )
-  secondExp = exp(  -.5* detCov(A)**(-1) * sum( (Ai*(centeri) * invdiagvar * (D-A*ddot)**2 / ((A*sddot)**2 + sD**2) )))
+  invdiagvar =as.numeric( ((Ai*sddoti)**2 + sDi**2)**(-1))
+  centeri = as.numeric((Di-Ai*ddoti)**2)
 
+  firstExp = exp( (-.5) * centeri *invdiagvar  )
+  secondExp = exp(  as.numeric(-.5* detCov(A)**(-1) * sum( (Ai*(centeri) * invdiagvar * (D-A*ddot)**2 / ((A*sddot)**2 + sD**2) ))))
 
-  return( detCov(A)**(-.5) * invdiagvar * firstExp * secondExp *(Ai > a) * (Ai < b) / Ai)
+  #
+  return( detCov(A)**(-.5)* invdiagvar * firstExp * secondExp *(Ai > a) * (Ai < b) / Ai )
 }
 
-#'@export
-logitT <- function(A, upperbound, lowerbound) {
 
+#=================================================================================@
+#'@export
+logitT <- function(A, bounds) {
+  lowerbound = bounds[1]
+  upperbound = bounds[2]
   return(log((A-lowerbound) / (upperbound-A)))
 }
 
+
+#=================================================================================@
+#' @export
+arctanT <- function(A, bounds) {
+  lowerbound = bounds[1]
+  upperbound = bounds[2]
+  return( atan( pi* (A-lowerbound) / (upperbound - lowerbound) - pi /2 ) )
+}
+
+#=================================================================================@
 #'@export
-GibbsSampler <- function(DataMeasures, init, sd, nchain, burnin, Sc,
-                      LowerPeriod, UpperPeriod, lag = 10) {
+initialize_SC <- function(Sc, LowerPeriod, UpperPeriod, plotGraph = F) {
+  Sc = Sc[-1, ] #del. first line
+  n = nrow(Sc)
+  rownames(Sc) = colnames(Sc) = paste0("A", 1:n)
+
+  network = igraph::graph_from_adjacency_matrix(Sc)
+  if (plotGraph) {
+    plot(network)
+  }
+  A = rep(NA, n)
+  ##topological order
+  topo_order = igraph::topo_sort(network, mode = "out")
+  for (i in topo_order) {
+    max_bound = UpperPeriod
+    min_bound = LowerPeriod
+    #MAJ max_bound
+    older_ages = A[which(Sc[i, ] == 1 )]
+    if ( any(!is.na(older_ages))) {
+      max_bound = min(max_bound, min(older_ages, na.rm = T))
+    }
+
+    #MAJ min_bound
+    younger_ages = A[which(Sc[, i] == 1)]
+    if (any(!is.na(younger_ages ))) {
+      min_bound = max(min_bound, max(younger_ages, na.rm =T))
+    }
+
+    ##simulation
+    if (min_bound < max_bound) {
+      A[i] = runif(1, min_bound, max_bound)
+    }
+
+    else { A[i] = min_bound}
+  }
+  return(A)
+}
+
+
+#=================================================================================@
+#'@export
+GibbsSampler <- function(DataMeasures,  sd, nchain, burnin, Sc,
+                      LowerPeriod, UpperPeriod, Transformation = "arctan", lag = 10, plotGraph = T) {
 
   n_ages = DataMeasures$Measures$Nb_sample
   chain = matrix(NA, nrow = nchain+1, ncol = n_ages)
-  ##logit transformation for init
+  A = chain #nchains+1 x n_ages
+  #initialize
+  init = initialize_SC(Sc, LowerPeriod, UpperPeriod, plotGraph)
 
-  chain[1, ] = sapply(init, logitT, upperbound = UpperPeriod, )
+  A[1, ] = init
+
+  acceptance = 0
+
+  IndexBounds = sapply(1:n_ages, findbound, Sc) + 1 # 2 x n_ages
+  IndexBounds[which(IndexBounds == 0, arr.ind = T)] = n_ages + 2
+  bounds = apply(IndexBounds, 2, function(b) c(LowerPeriod, init, UpperPeriod)[b])
+
+  if (Transformation == "logit") { #Test Sigmoid with parameter lambda
+  ##logit transformation for init
+  chain[1, ] = sapply(init, logitT, bounds) # n_ages
+  }
+
+  else if (Transformation == "arctan") {
+    chain[1, ] = sapply(init, arctanT, bounds)
+  }
 
   for (iter in 1:nchain) {
     for (i in 1:n_ages) {
+      bounds_i = c(LowerPeriod, chain[iter, ], UpperPeriod)[IndexBounds[, (i+1)]] # (a(iter), b(iter))
+
+      #proposal depends on the transformation
+      if (Transformation == "arctan") {
+      # MAJ Ai
+      proposal = chain[iter, i] + truncnorm::rtruncnorm(1, a = -pi/2, b = pi/2, sd = sd[i]) #proposal for the i-th Age in (-pi, pi)/2
+      Aproposal = (bounds_i[2] - bounds_i[1]) * tan(proposal)/pi + (bounds_i[2] + bounds_i[1])/2
+
+      top = GibbsDensity(DataMeasures, A[iter, ], Aproposal, i, bounds_i[1], bounds_i[2]) * (1+tan(proposal)**2)
+      bottom = GibbsDensity(DataMeasures, A[iter, ], A[iter, i], i, bounds_i[1], bounds_i[2]) * (1+ tan(chain[iter, i])**2)
+
+      log_ratio = log(top) - log(bottom)
+
+      u = log(runif(1))
+
+      print(top)
+
+      if (u < log_ratio) {
+        chain[iter+1, i] = proposal
+        A[iter+1, i] = Aproposal
+        acceptance = acceptance +1
+      }
+
+      else {
+        chain[iter+1, i] = chain[iter, i]
+        A[iter+1, i] = A[iter, i]
+        }
+      }##### END OF ARCTAN TRANSFORMATION
+
 
     }
-
-
-
+  ## message
+    if (iter%/% 1000) {
+      message(paste("Iteration iter =", iter, "done"))
+    }
   }
+  seq_lag = seq(burnin, (nchain+1), lag)
+  chain = chain[seq_lag, ]
+  A = A[seq_lag, ]
+
+  ##plot
+
+  return(list(A = A, chain = chain, acceptance = acceptance / (n_ages * nchain)))
 }
 
 
