8550fe64c6d4543cd5703858bb0e945570b779b9
imn167 <imn167.tb@gmail.com>
2025-10-01 10:59:24 +0200

    diagnostic for macOS

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 library(testthat)
 library(BayLumPlus)
 
-# Set number of parallel workers to available cores minus 1
-library(parallel)
-num_cores <- max(1, parallel::detectCores() - 1)
-Sys.setenv("TESTTHAT_PARALLEL" = "true")
-Sys.setenv("TESTTHAT_CPUS" = num_cores)
+# # Set number of parallel workers to available cores minus 1
+# library(parallel)
+# num_cores <- max(1, parallel::detectCores() - 1)
+# Sys.setenv("TESTTHAT_PARALLEL" = "true")
+# Sys.setenv("TESTTHAT_CPUS" = num_cores)
 
 test_check("BayLumPlus")
