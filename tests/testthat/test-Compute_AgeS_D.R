83a5ded2685c315a04db7b43eb79dc2cb1590756
imn167 <imn167.tb@gmail.com>
2025-10-02 13:23:15 +0200

    Fix error in test jags_method

--- tests/testthat/test-Compute_AgeS_D.R
+++ tests/testthat/test-Compute_AgeS_D.R
@@ -1,10 +1,10 @@
 #Mock data creations helpers
-skip_on_ci()
+
 
 test_that("Full function test", {
 dt <- list(D = OSLJingbian$D, sD = OSLJingbian$sD, ddot = OSLJingbian$ddot)
   expect_s3_class(suppressWarnings(Compute_AgeS_D(DATA = dt, Nb_sample = OSLJingbian$Nb_Sample,
-                                  SampleNames = OSLJingbian$SampleNames, ThetaMatrix = OSLJingbian$ThetaMatrix,PriorAge = rep(c(1, 1400), OSLJingbian$Nb_Sample),
+                                  SampleNames = OSLJingbian$SampleNames, ThetaMatrix = OSLJingbian$ThetaMatrix,PriorAge = rep(c(1, 1400), OSLJingbian$Nb_Sample), jags_method = "rjags",
                                  prior = "Independance", Iter = 100, adapt = 100, burnin = 50, n.chains = 2, quiet = TRUE)),
                   class = "BayLum.list")
 
--- tests/testthat/test-IsotonicCurve.R
+++ tests/testthat/test-IsotonicCurve.R
@@ -1,4 +1,3 @@
-skip_on_ci()
 
 
 if (capabilities("cairo")) {
